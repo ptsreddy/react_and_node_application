9a13b2a353b5dd41daae6dfcac9c664a7e9e6449
ptsreddy <55979262+ptsreddy@users.noreply.github.com>
2023-12-15 20:07:51 +0530

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import ExampleComponent from "./components/ExampleComponent";
+import thisExampleComponent from "./components/ExampleComponent";
 
 ReactDOM.render(<ExampleComponent />, document.getElementById("container"));
